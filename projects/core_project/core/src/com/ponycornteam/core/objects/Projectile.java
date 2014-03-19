30c86fd5b21b8ae4c804745c7ce52a9bcedbcac8
Bawaaaaah <limason51@gmail.com>
2014-03-19 23:59:34 +0100

    implement IColidable change owner type form Character to ICharacter

--- projects/core_project/core/src/com/ponycornteam/core/objects/Projectile.java
+++ projects/core_project/core/src/com/ponycornteam/core/objects/Projectile.java
@@ -4,17 +4,19 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.ponycornteam.tools.Coord;
 import com.ponycornteam.tools.Coord.direction;
 
-public class Projectile implements IDrawable {
+public class Projectile implements IDrawable, IColidable {
 	public Coord localCoord;
 	public Sprite texture;
 	public double speed;
 	protected int width, heigth;
-	public Character owner = null;
+	public ICharacter owner = null;
 	public Sound shootSound;
 	public Sound bounceSound;
+	public Boolean isDestroy = false;
 
 	private void avancer(float deltaTime) {
 		if (speed > 0) {
@@ -26,15 +28,39 @@ public class Projectile implements IDrawable {
 		}
 	}
 	
-	public void colide(Coord.direction dir){
-		speed /=2;
-		localCoord.angle = ((dir == direction.top||dir == direction.bot)?360:180)-localCoord.angle;
-	}
-	
 	public void draw(SpriteBatch batch, float stateTime) {
 		avancer(Gdx.graphics.getDeltaTime());
 		texture.setPosition((float) localCoord.x, (float) localCoord.y);
 		texture.setRotation((float) -localCoord.angle);
 		texture.draw(batch);
+	}
+
+	@Override
+	public void setX(double x) {
+		localCoord.x = x;
+		
+	}
+
+	@Override
+	public void setY(double y) {
+		localCoord.y = y;
+		
+	}
+
+	@Override
+	public Rectangle getRectangle() {
+		return new Rectangle((float)localCoord.x,(float)localCoord.y,(float)texture.getWidth(),(float)texture.getHeight());
+	}
+
+	@Override
+	public void colisionObject(direction dir) {
+		speed /=2;
+		localCoord.angle = ((dir == direction.top||dir == direction.bot)?360:180)-localCoord.angle;
+	}
+
+	@Override
+	public void colisionProjectile(Projectile projectile) {
+		// TODO Auto-generated method stub
+		
 	}
 }
