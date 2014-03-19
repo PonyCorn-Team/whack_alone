ed6f0d9b6e59555aee57575bc97e68c89d327547
Bawaaaaah <limason51@gmail.com>
2014-03-19 11:01:53 +0100

    Maintenant utiliser le getdelta de gdx partout mise a jour des projectile et plein d'autre petite modif liée au projectile

--- core_project/core/src/com/ponycornteam/core/objects/Projectile.java
+++ core_project/core/src/com/ponycornteam/core/objects/Projectile.java
@@ -1,5 +1,6 @@
 package com.ponycornteam.core.objects;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ponycornteam.tools.Coord;
@@ -10,22 +11,23 @@ public class Projectile implements IDrawable {
 	public double speed;
 	protected int width, heigth;
 	public Character owner = null;
+	private float oldTime = 0;
 
 	private void avancer(float deltaTime) {
 		if (speed > 0) {
-			localCoord.x += Math.cos(localCoord.angle / 180 * Math.PI) * speed
+			localCoord.x += Math.cos(-localCoord.angle / 180 * Math.PI) * speed
 					* deltaTime;
-			localCoord.y += Math.sin(localCoord.angle / 180 * Math.PI) * speed
+			localCoord.y += Math.sin(-localCoord.angle / 180 * Math.PI) * speed
 					* deltaTime;
-			speed -= 9.81 * deltaTime;
+			speed -= 100 * deltaTime;
 		}
 	}
 
 	@Override
 	public void draw(SpriteBatch batch, float stateTime) {
-		avancer(stateTime);
+		avancer(Gdx.graphics.getDeltaTime());
 		texture.setPosition((float) localCoord.x, (float) localCoord.y);
-		texture.setRotation((float) localCoord.angle);
+		texture.setRotation((float) -localCoord.angle);
 		texture.draw(batch);
 	}
 }
