b1847bea2c15fafa4f98ce8212a110f213fe50a9
Bawaaaaah <limason51@gmail.com>
2014-03-20 11:22:30 +0100

    add shot option and ammo gestion and move colisionProjectile methode respectivly into player and ennemy class

--- core_project/core/src/com/ponycornteam/core/objects/Player.java
+++ core_project/core/src/com/ponycornteam/core/objects/Player.java
@@ -4,15 +4,22 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.ponycornteam.core.GameScreen;
+import com.ponycornteam.core.objects.Projectile.Ammo;
 import com.ponycornteam.tools.Coord;
 
 public class Player extends Character {
-	
-	public Player(Texture standing, Coord coord) {
+
+	public Player(Texture standing, Coord coord, Ammo ammotype, Sprite texture) {
 		localCoord = coord;
 		this.standing = new Sprite(standing);
 		width = standing.getWidth();
 		heigth = standing.getHeight();
+
+		Projectile pro = new Projectile();
+		pro.ammoType = ammotype;
+		pro.texture = texture;
+		ammo.add(pro);
 	}
 
 	public void setMoving(Boolean move) {
@@ -22,12 +29,47 @@ public class Player extends Character {
 	public void draw(SpriteBatch batch, float stateTime, Boolean move) {
 		this.move = move;
 		draw(batch, stateTime);
-		
+
 	}
-	public void setText(String txt, Color txtColor, Double timeShowing)
-	{
+
+	public void setText(String txt, Color txtColor, Double timeShowing) {
 		saying = txt;
 		saycolor = txtColor;
-		sayingCount = (timeShowing>0)?timeShowing:5;
+		sayingCount = (timeShowing > 0) ? timeShowing : 5;
+	}
+
+	public void shoot() {
+		if (ammo.size > 0) {
+			Projectile pro = ammo.pop();
+			pro.localCoord = new Coord(0,0);
+			pro.localCoord.x = localCoord.x + Math.cos(-localCoord.angle / 180 * Math.PI) * (standing.getWidth());
+			pro.localCoord.y = localCoord.y + Math.sin(-localCoord.angle / 180 * Math.PI) * (standing.getHeight()/2);
+			pro.localCoord.angle = localCoord.angle;
+			pro.speed = 600;
+			pro.owner = this;
+			GameScreen.projectiles.add(pro);
+		}
+	}
+
+	@Override
+	public void colisionProjectile(Projectile projectile) {
+		if (projectile.owner != this && projectile.owner != null && projectile.speed > 0) {
+			saying = "shit i'm dead!";
+			saycolor = Color.RED;
+			sayingCount = 10.0;
+		} else {
+			if (projectile.ammoType == Ammo.palet && projectile.speed < 250) {
+				saying = "YEAH AMMO!";
+				saycolor = Color.PINK;
+				sayingCount = 2.5;
+				projectile.localCoord = null;
+				ammo.add(projectile);
+			} else {
+				saying = "pfiou it's mine!";
+				saycolor = Color.GREEN;
+				sayingCount = 2.5;
+			}
+		}
+
 	}
 }
